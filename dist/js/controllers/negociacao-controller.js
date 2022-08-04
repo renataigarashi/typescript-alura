3c187fba072eeb9b1c3435fdb8345086b5208306
Renata <renata.sayuri@dasa.com.br>
2022-08-04 18:07:47 -0300

    criado enums para dias da semana

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -13,15 +14,18 @@ export class NegociacaoController {
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        // getDay são os dias da semana, começa no 0 (domingo) e vai até 6 (sabado)
-        if (negociacao.data.getDay() > 0 && negociacao.data.getDay() < 6) { // vai salvar os dados dos dias uteis
-            this.negociacoes.adiciona(negociacao);
-            this.limparFormulário();
-            this.atualizarView();
-        }
-        else {
+        if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
+            return;
         }
+        this.negociacoes.adiciona(negociacao);
+        this.limparFormulário();
+        this.atualizarView();
+    }
+    ehDiaUtil(data) {
+        // getDay são os dias da semana, começa no 0 (domingo) e vai até 6 (sabado)
+        return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
+        // vai validar os dias uteis
     }
     criaNegociacao() {
         const exp = /-/g; //encontrar o hifen e o 'g' é de global, pra encontrar todos os hifens
